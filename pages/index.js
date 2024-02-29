965994a45f1db7422ca9c0933ade16924fbda3de
Jangler19 <98561837+Jangler19@users.noreply.github.com>
2024-02-29 13:29:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,15 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        {/* Player de vídeo */}
+        <div>
+          <h2>Player de Vídeo</h2>
+          <iframe name="Player" src="https://sinalpublico.com/player3/server8http2hlb.php?vid=FRTSDUNVRSOT01EP01" frameborder="0" height="400" scrolling="no" width="640" allow="encrypted-media" allowFullScreen></iframe>
+        </div>
       </main>
 
       <Footer />
     </div>
   )
-}
+          }
